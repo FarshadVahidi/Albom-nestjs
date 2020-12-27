98eb8010d076c846da214fe2ed8aaa4efd1c3eaf
Farshad Vahidifard <farshad.vahidifard@studenti.unimi.it>
2020-12-27 23:25:42 +0100

    add modification for db mysql

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,22 @@ import { AppService } from './app.service';
 import { PersonsController } from './persons/persons.controller';
 import { PersonsService } from './persons/persons.service';
 import { PersonsModule } from './persons/persons.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [PersonsModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'farshad',
+      password: 'password',
+      database: 'albom',
+      entities: [],
+      synchronize: true,
+    }),
+    PersonsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
